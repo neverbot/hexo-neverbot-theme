7776a536f03c1c65e16f3c5866d37319f86587d7
neverbot <ivan@neverbot.com>
2023-10-17 17:24:42 +0200

    feat: minimal function to create badges

---
+++ scripts/badge.js
@@ -0,0 +1,5 @@
+hexo.extend.tag.register('badge', function (args) {
+  const [color, content] = args
+
+  return '<span class="badge ' + color + '">' + content + '</span>'
+}, { ends: false, async: false })
