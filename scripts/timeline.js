368ee5f8a29ba3a0b58e28bccf60d0bbbcf8c4d3
neverbot <ivan@neverbot.com>
2023-10-18 14:32:54 +0200

    chore: fix in timelines js (allow empty times)

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -4,7 +4,7 @@ hexo.extend.tag.register('year', function (args, content) {
   return '<div class="timeline-node">' +
           '<div class="timeline-version">' + up + '</div>' +
           '<div class="timeline-body">' +
-            '<div class="timeline-time">' + left + '</div>' +
+            (left.trim().length ? '<div class="timeline-time">' + left + '</div>' : '') +
             (!last ? '<div class="timeline-line"></div>' : '') +
             '<div class="timeline-circle"></div>' +
             '<div class="timeline-title">' + hexo.render.renderSync({ text: right, engine: 'markdown' }) + '</div>' +
