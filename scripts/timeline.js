e49110cba2871358936787c003d71ef8f15b7aa3
neverbot <ivan@neverbot.com>
2024-01-10 18:35:28 +0100

    feat: improve timelines

--- scripts/timeline.js
+++ scripts/timeline.js
@@ -1,13 +1,15 @@
-hexo.extend.tag.register('year', function (args, content) {
-  const [left, up, right, last] = args
+/* eslint-disable */
 
-  return '<div class="timeline-node">' +
+hexo.extend.tag.register('year', function (args, content) {
+  const [badge, title, subtitle, last] = args
+
+  return '<div class="timeline-node' + (last ? ' last' : '') + '">' +
           '<div class="timeline-line"></div>' +
-          '<div class="timeline-over">' + up + '</div>' +
-          (left.trim().length ? '<div class="timeline-time">' + left + '</div>' : '') +
+          (badge.trim().length ? '<div class="timeline-time">' + badge + '</div>' : '') +
           '<div class="timeline-circle"></div>' +
           '<div class="timeline-body">' +
-            '<div class="timeline-title">' + hexo.render.renderSync({ text: right, engine: 'markdown' }) + '</div>' +
+            '<div class="timeline-title">' + hexo.render.renderSync({ text: title, engine: 'markdown' }) + '</div>' +
+            '<div class="timeline-subtitle">' + hexo.render.renderSync({ text: subtitle, engine: 'markdown' }) + '</div>' +
             '<div class="timeline-content">' + hexo.render.renderSync({ text: content, engine: 'markdown' }) + '</div>' +
           '</div>' +
         '</div>'
