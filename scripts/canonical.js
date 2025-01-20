5170c68d6d4bcf7d25cf424c8be0d7cf0f0bfb85
neverbot <ivan@neverbot.com>
2025-01-20 16:32:38 +0100

    better canonical url generation

--- scripts/canonical.js
+++ scripts/canonical.js
@@ -11,8 +11,11 @@
 // });
 
 hexo.extend.helper.register('autoCanonical', function (config, page) {
-  var base_url = config.url;
-  if (config.url.charAt(config.url.length - 1) !== '/') base_url += '/';
+  let base_url = config.url;
+
+  // add slash to the base url, except in the home (root dir)
+  if (base_url.charAt(base_url.length - 1) !== '/' && page.canonical_path != 'index.html')
+    base_url += '/';
 
   return (
     '<link rel="canonical" href="' +
